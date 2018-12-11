e3ec213476288ac84c9bce883ed10a7f610bc21e
PUGE <100284685@qq.com>
2018-12-11 23:17:35 +0800

    添加用户窗口

--- src/views/User.vue
+++ src/views/User.vue
@@ -14,7 +14,7 @@
       </div>
       <div class="content">
         <div class="content-bar">
-          <div class="add-user button">添加用户</div>
+          <div class="add-user button" @click="shouAddBox = true">添加用户</div>
           <div class="delete-user button">删除用户</div>
         </div>
         <div class="table-box">
@@ -44,6 +44,23 @@
             条
           </div>
           <Paging class="page-box-right" :page-index="1" :total="100" :page-size="20" @change="pageChange"></Paging>
+        </div>
+      </div>
+    </div>
+    <div class="show-box" v-show="shouAddBox">
+      <div class="show-box-body">
+        <div class="title">添加</div>
+        <div class="input-box">
+          <div class="text">用户名:</div>
+          <input type="text"/>
+        </div>
+        <div class="input-box">
+          <div class="text">密码:</div>
+          <input type="text"/>
+        </div>
+        <div class="button-box">
+          <div class="button cancel" @click="shouAddBox = false">取消</div>
+          <div class="button submit" @click="addNewUser">提交</div>
         </div>
       </div>
     </div>
@@ -83,12 +100,18 @@ export default {
         { isCheck: false, id: 1, name: "测试", state: "在线" },
         { isCheck: false, id: 1, name: "测试", state: "在线" }
       ],
-      isCheckAll: false
+      isCheckAll: false,
+      shouAddBox: false
     }
   },
   methods: {
     pageChange () {
 
+    },
+    // 添加新用户
+    addNewUser () {
+      console.log('添加新用户!')
+      this.shouAddBox = false
     }
   }
 }
@@ -207,6 +230,63 @@ table {
       text-align: center;
       border-radius: 5px;
       border: 1px solid #ccc;
+    }
+  }
+}
+.show-box {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  .show-box-body {
+    width: 470px;
+    height: 260px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    background-color: white;
+    .title {
+      background-color: #6e6e6e;
+      color: white;
+      text-align: left;
+      padding: 0 10px;
+      height: 45px;
+      line-height: 45px;
+    }
+    .input-box {
+      display: flex;
+      justify-content: center;
+      margin: 30px 0;
+      line-height: 30px;
+      .text {
+        width: 80px;
+      }
+      input {
+        height: 25px;
+        width: 240px;
+      }
+    }
+    .button-box {
+      display: flex;
+      justify-content: flex-end;
+      margin: 0 40px;
+      .button {
+        background-color: #595959;
+        color: white;
+        width: 80px;
+        height: 40px;
+        text-align: center;
+        line-height: 40px;
+        margin: 0 5px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
     }
   }
 }
